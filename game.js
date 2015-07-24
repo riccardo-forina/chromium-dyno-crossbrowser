f4f40bb62709884740a73e3d68287e3eba7883a4
Riccardo Forina <r.forina@gmail.com>
2015-07-25 00:38:53 +0200

    Fix iOS detection

--- game.js
+++ game.js
@@ -84,8 +84,7 @@ var FPS = 60;
 var IS_HIDPI = window.devicePixelRatio > 1;
 
 /** @const */
-var IS_IOS = window.navigator.userAgent.indexOf('CriOS') > -1 ||
-    window.navigator.userAgent == 'UIWebViewForStaticFileContent';
+var IS_IOS = !!navigator.platform.match(/iPhone|iPod|iPad/);
 
 /** @const */
 var IS_MOBILE = window.navigator.userAgent.indexOf('Mobi') > -1 || IS_IOS;
